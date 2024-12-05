5c11c0dd225166db454a9d33e6ac9989bf19ab4b
xinchen <564368550@qq.com>
2024-12-05 22:12:40 +0800

    finish 03_ticket_v1

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,60 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+
+pub struct Order {
+    product_name: String,
+    quantity: i32,
+    unit_price: i32,
+}
+
+impl Order {
+    pub fn new(product_name: String, quantity: i32, unit_price: i32) -> Order {
+        if product_name.is_empty() || product_name.capacity() > 300 {
+            panic!("Product name must not be empty or longer than 300 bytes");
+        }
+        if quantity <= 0 {
+            panic!("Quantity must be strictly greater than zero");
+        }
+        if unit_price <= 0 {
+            panic!("Unit price must be strictly greater than zero");
+        }
+        Order {
+            product_name,
+            quantity,
+            unit_price,
+        }
+    }
+
+    pub fn product_name(&self) -> &str {
+        &self.product_name
+    }
+    pub fn quantity(&self) -> &i32 {
+        &self.quantity
+    }
+    pub fn unit_price(&self) -> &i32 {
+        &self.unit_price
+    }
+    pub fn total(&self) -> i32 {
+        self.quantity * self.unit_price
+    }
+
+    pub fn set_product_name(&mut self, product_name: String) {
+        if product_name.is_empty() || product_name.capacity() > 300 {
+            panic!("Product name must not be empty or longer than 300 bytes");
+        }
+        self.product_name = product_name;
+    }
+    pub fn set_quantity(&mut self, quantity: i32) {
+        if quantity <= 0 {
+            panic!("Quantity must be strictly greater than zero");
+        }
+        self.quantity = quantity;
+    }
+    pub fn set_unit_price(&mut self, unit_price: i32) {
+        if unit_price <= 0 {
+            panic!("Unit price must be strictly greater than zero");
+        }
+        self.unit_price = unit_price;
+    }
+}
